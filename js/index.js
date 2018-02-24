b516607a15a92e4e0b6f4d4e9942b3b46649bee2
Alger Makiputin <algerzxc@gmail.com>
2018-02-24 22:00:28 +0800

    asd

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 	var btn = $('#btn');
 	var query;
 	var placeID;
+	
 
 	$("#guide-link, #feedback-link").click(function(){
 		$("#page").removeClass('hide');
@@ -109,12 +110,123 @@ $(document).ready(function(){
 	  
 	}
 
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position){
+	    	lat = position.coords.latitude; 
+			long = position.coords.longitude; 
+			
+	    })
+	}
+
+	function initbg() {
+        var map_canvas = document.getElementById('map_canvas');
+        var map_options = {
+          center: new google.maps.LatLng(44.5403, -78.5400),
+          zoom: 12,
+        
+          disableDefaultUI: true,
+          scrollwheel: false,
+          styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}],
+
+            },
+            {
+			    "featureType": "road",
+			    "elementType": "labels",
+			    "stylers": [
+			      { "visibility": "off" }
+			    ]
+			  },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+        }
+        var map = new google.maps.Map(map_canvas, map_options)
+      }
+      google.maps.event.addDomListener(window, 'load', initbg);
+  		clearMarkers();
 	function initialize(lat,long) {
 	  var pyrmont = new google.maps.LatLng(lat,long);
 
 	  map = new google.maps.Map(document.getElementById('map'), {
 	      center: pyrmont,
-	      zoom: 15
+	      zoom: 15,
+	      disableDefaultUI: true,
+          scrollwheel: false
 	    });
 
 	  var request = {
@@ -203,7 +315,9 @@ $(document).ready(function(){
         var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
           map: map,
-          position: place.geometry.location
+          position: place.geometry.location,
+          disableDefaultUI: true,
+          scrollwheel: false
         });
 
         google.maps.event.addListener(marker, 'click', function() {
